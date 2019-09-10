ad174dceadf64e5d7444b7601a7f24682c1e6a78
bart7801 <bart7801@o2.pl>
2019-09-10 14:00:41 +0200

    all 4 language

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,8 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Bonjour tout le monde");
-        System.out.println("Hallo Welt");
+        System.out.println("Hello World!");
+        System.out.println("Bonjour tout le monde!");
+        System.out.println("Hallo Welt!");
         System.out.println("Halo świecie!");
 
     }
